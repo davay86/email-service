329b9586d8976fdf24cdc02f337117062c88a42b
SleepingTalent <jaybono30@hotmail.com>
2017-09-28 13:44:22 +0100

    EM-1: added spring banner

--- email-service-parent/email-service/src/main/java/com/babcock/email/service/EmailService.java
+++ email-service-parent/email-service/src/main/java/com/babcock/email/service/EmailService.java
@@ -1,8 +1,21 @@
 package com.babcock.email.service;
 
+import com.babcock.email.stream.payload.User;
+import com.babcock.email.stream.payload.UserPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    private static Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    public void sendUserCreationEmail(UserPayload userPayload) {
+
+        for(User user : userPayload.getUsers()) {
+            logger.info("Sending Email : "+ user);
+        }
+
+    }
 }
--- email-service-parent/email-service/src/main/java/com/babcock/email/stream/listener/NotifyUserCreationListener.java
+++ email-service-parent/email-service/src/main/java/com/babcock/email/stream/listener/NotifyUserCreationListener.java
@@ -1,9 +1,11 @@
 package com.babcock.email.stream.listener;
 
+import com.babcock.email.service.EmailService;
 import com.babcock.email.stream.channel.MessageChannels;
 import com.babcock.email.stream.payload.UserPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Component;
@@ -14,8 +16,12 @@ public class NotifyUserCreationListener {
 
     private static Logger logger = LoggerFactory.getLogger(NotifyUserCreationListener.class);
 
+    @Autowired
+    EmailService emailService;
+
     @StreamListener(MessageChannels.NOTIFY_USER_CREATION_CHANNEL)
     public void notifyUserCreationReceiver(UserPayload userPayload) {
         logger.info("UserPayload Received : "+ userPayload);
+        emailService.sendUserCreationEmail(userPayload);
     }
 }
