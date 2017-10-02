e31cfcf1ee4df204fa97cf1a17a31dd80c60d5ef
SleepingTalent <jaybono30@hotmail.com>
2017-10-02 14:56:48 +0100

    EM-1: updated hystrix command annotation

--- email-service-parent/email-service/src/main/java/com/babcock/email/stream/listener/NotifyUserCreationListener.java
+++ email-service-parent/email-service/src/main/java/com/babcock/email/stream/listener/NotifyUserCreationListener.java
@@ -3,6 +3,7 @@ package com.babcock.email.stream.listener;
 import com.babcock.email.service.EmailService;
 import com.babcock.email.stream.channel.MessageChannels;
 import com.babcock.email.stream.payload.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,13 @@ public class NotifyUserCreationListener {
 
     private static Logger logger = LoggerFactory.getLogger(NotifyUserCreationListener.class);
 
-    @Autowired
-    EmailService emailService;
-
+    @HystrixCommand
     @StreamListener(MessageChannels.NOTIFY_USER_CREATION_CHANNEL)
     public void notifyUserCreationReceiver(User userPayload) {
         logger.info("UserPayload Received : "+ userPayload);
         emailService.sendUserCreationEmail(userPayload);
     }
+
+    @Autowired
+    EmailService emailService;
 }
