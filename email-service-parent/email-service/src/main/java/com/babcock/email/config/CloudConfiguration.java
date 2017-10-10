4d523d7bb2fdf568bbf2cee6908702ea75ebe2f2
SleepingTalent <jaybono30@hotmail.com>
2017-10-10 08:45:40 +0100

    removed unused imports

--- email-service-parent/email-service/src/main/java/com/babcock/email/config/CloudConfiguration.java
+++ email-service-parent/email-service/src/main/java/com/babcock/email/config/CloudConfiguration.java
@@ -1,13 +1,9 @@
 package com.babcock.email.config;
 
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableCircuitBreaker
