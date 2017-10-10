410b1f28cc9a03477098f0e35eb1eac723641d92
SleepingTalent <jaybono30@hotmail.com>
2017-10-10 08:20:16 +0100

    refactored waitForService class

--- email-service-parent/email-service-test/src/test/java/com/babcock/integration/asserter/WaitForService.java
+++ email-service-parent/email-service-test/src/test/java/com/babcock/integration/asserter/WaitForService.java
@@ -1,6 +1,7 @@
 package com.babcock.integration.asserter;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 public class WaitForService extends WaitUntilAsserter {
@@ -18,16 +19,17 @@ public class WaitForService extends WaitUntilAsserter {
     protected boolean execute() {
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
             if(response.getStatusCode().is2xxSuccessful()) {
                 return true;
             }else {
                 System.out.print(".");
                 return false;
             }
-
-        }catch (Exception ex) {
+        }catch (ResourceAccessException ex) {
             System.out.print(".");
+            return false;
+        }catch (Exception ex) {
+            System.err.println("Service Not Found Retrying : "+ex);
             return false;
         }
     }
